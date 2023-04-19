4ad1b154bf914805ab85e082815c4ec19897927c
ayushpatel <ayushpatel4747@gmail.com>
2023-04-19 08:31:21 -0700

    Initial Commit

---
+++ Foodi/FoodiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiApp.swift
+//  Foodi
+//
+//  Created by Ayush Patel on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
